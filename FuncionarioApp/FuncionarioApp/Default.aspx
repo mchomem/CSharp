9275f85f993fb5e5f5b6ca60d5577b2608824b10
Misael <misael.homem@gmail.com>
2016-06-19 17:01:06 -0300

    Client to consume data of WebService.

---
+++ FuncionarioApp/FuncionarioApp/Default.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FuncionarioApp.Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <script type="text/javascript" src="Scripts/jquery/jquery-1.12.4.js"></script>
+    <script type="text/javascript" src="Scripts/Default.js"></script>
+    <link href="Styles/Default.css" rel="stylesheet" />
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <fieldset>
+        <legend>Pesquisar Funcionários</legend>
+        <table>
+            <thead></thead>
+            <tbody>
+                <tr>
+                    <td>Sexo</td>
+                    <td>
+                        <asp:DropDownList ID="ddlSexo" runat="server">
+                            <asp:ListItem Value=""></asp:ListItem>
+                            <asp:ListItem Value="M">Masculino</asp:ListItem>
+                            <asp:ListItem Value="F">Feminino</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                    <td>
+                        <asp:Button ID="btnPesquisar" runat="server" Text="Pesquisar (XML)" OnClick="btnPesquisar_Click" />
+                    </td>
+                    <td>
+                        <input id="btnPesquisarJSON" type="button" value="Pesquisar (JSON)" onclick="" />
+                    </td>
+                </tr>
+            </tbody>
+            <tfoot></tfoot>
+        </table>
+    </fieldset>
+
+    <div>
+        <asp:GridView ID="gvwFuncionarios" runat="server" AutoGenerateColumns="false" Width="100%">
+            <Columns>
+                <asp:BoundField HeaderText="ID"               DataField="idt_func" />
+                <asp:BoundField HeaderText="CPF"              DataField="cpf_func" />
+                <asp:BoundField HeaderText="Nome"             DataField="nom_func" />
+                <asp:BoundField HeaderText="RG"               DataField="ci_func" />
+                <asp:BoundField HeaderText="Email"            DataField="email_func" />
+                <asp:BoundField HeaderText="Tel. Residencial" DataField="tel_res_func" />
+                <asp:BoundField HeaderText="Tel. Celular"     DataField="tel_cel_func" />
+                <asp:BoundField HeaderText="Data Nasc."       DataField="dat_nasc_func" />
+                <asp:BoundField HeaderText="Sexo"             DataField="sexo_func" />
+            </Columns>
+        </asp:GridView>
+    </div>
+
+    <div id="divJSON"></div>
+</asp:Content>
---
+++ FuncionarioApp/FuncionarioApp/Scripts/Default.js
@@ -0,0 +1,89 @@
+﻿$(document).ready(function() {
+
+    $('#btnPesquisarJSON').click(function () {        
+
+        var parans = '{ sexo: ' + '\'' + $('[id*=ddlSexo]').val() + '\' }';
+        getData('http://localhost:81/Funcionarios/Funcionarios.asmx/ObterFuncionariosPorSexoJSON', parans);
+
+    });
+
+});
+
+
+function getData(pUrl, parans) {
+
+    $.ajax({
+        type:          'POST'
+        , url:         pUrl
+        , contentType: 'application/json; charset=utf-8'
+        , dataType:    'json'
+        , data:        parans
+        , success:     function (data, status) {
+            
+            var info = $.parseJSON(data.d);
+
+            setDataGrid(info);
+
+        }
+        , error:       function (xmlHttpRequest, status, error) {
+            alert('Falha: ' + error);
+        }
+    });
+
+}
+
+function setDataGrid(infoJSON) {
+
+    // Remove a gridJSON.
+    if ($('#gridJSON').length > 0) {
+        $('#gridJSON').remove();
+    }
+
+    // Remove a GridView .NET
+    if ($('[id*=gvwFuncionarios]').length > 0) {
+        $('[id*=gvwFuncionarios]').remove();
+    }
+
+    var grid = '';
+    var data = '';
+
+    for (var i = 0; i < infoJSON.length; i++) {
+
+        data += '<tr>';
+        data +=     '<td>' + infoJSON[i].idt_func      + '</td>'
+        data +=     '<td>' + infoJSON[i].cpf_func      + '</td>'
+        data +=     '<td>' + infoJSON[i].nom_func      + '</td>'
+        data +=     '<td>' + infoJSON[i].ci_func       + '</td>'
+        data +=     '<td>' + infoJSON[i].email_func    + '</td>'
+        data +=     '<td>' + infoJSON[i].tel_res_func  + '</td>'
+        data +=     '<td>' + infoJSON[i].tel_cel_func  + '</td>'
+        data +=     '<td>' + infoJSON[i].dat_nasc_func + '</td>'
+        data +=     '<td>' + infoJSON[i].sexo_func     + '</td>'
+        data += '</tr>';
+
+    }
+
+    grid += '<table id="gridJSON" style="font-size: 14px; border-collapse: collapse; border: 1px solid black; width: 100%;">';
+    grid +=     '<thead style="border-bottom: 1px solid black;">';
+    grid +=         '<tr>';
+    grid +=             '<th>ID</th>'
+    grid +=             '<th>CPF</th>'
+    grid +=             '<th>Nome</th>'
+    grid +=             '<th>RG</th>'
+    grid +=             '<th>Email</th>'
+    grid +=             '<th>Tel. Residencial</th>'
+    grid +=             '<th>Tel. Celular</th>'
+    grid +=             '<th>Data Nasc.</th>'
+    grid +=             '<th>Sexo</th>'
+    grid +=         '</tr>';
+    grid +=     '</thead>';
+    grid +=     '<tbody>';
+    grid +=         data;
+    grid +=     '</tbody>';
+    grid +=     '<tfoot>';
+    grid +=     '</tfoot>';
+    grid += '</table>';
+
+    $('#divJSON').append(grid);
+
+}
